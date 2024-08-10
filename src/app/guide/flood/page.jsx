0fbfaa17ae2c946ab4d0290db9006f7aab44fcd8
protocols12 <teamprotocolpov@gmail.com>
2024-08-10 21:10:01 +0530

    Update page.jsx

--- src/app/guide/flood/page.jsx
+++ src/app/guide/flood/page.jsx
@@ -49,7 +49,7 @@ const Floods = () => {
         </ul>
         <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
           <Image
-            src="/images/floods.jpg" // Path to your image file in the public/images directory
+            src="/images/fldimg.jpg" // Path to your image file in the public/images directory
             alt="Flood Safety Tips"
             width={560} // Set the width of the image
             height={300} // Set the height of the image
